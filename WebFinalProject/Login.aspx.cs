7a6ed97494bbb12a25c20caf6b939f998b783e34
yeciin <yasshafez@gmail.com>
2024-01-08 21:52:27 +0200

    removed unnecessary imports

--- WebFinalProject/Login.aspx.cs
+++ WebFinalProject/Login.aspx.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Web;
 using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebFinalProject
 {
